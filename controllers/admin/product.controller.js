7feab96e142197936bad929b62c4e7a90909c732
Minh Dat <ngominhdat2004@gmail.com>
2026-02-10 01:17:19 +0700

    Lấy data sản phẩm in ra giao diện

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,15 @@
+const Product = require("../../models/product.model")
+
+//[GET] /admin/products
+module.exports.index = async (req, res) =>{
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products)
+
+    res.render("admin/pages/products/index", {
+        pageTitle: "Danh Sách Sản Phẩm",
+        products: products
+    })
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,13 @@
 
 const systemConfig = require("../../config/system");
 const dashboardRoute = require("./dashboard.route") 
+const productRoutes = require("./product.route") 
+
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAmin;
-  app.use(PATH_ADMIN + "/dashboard", dashboardRoute); 
+
+  app.use(PATH_ADMIN + "/dashboard", dashboardRoute)
+
+  app.use(PATH_ADMIN + "/products", productRoutes); 
 }
