ada101fb799212c6941150fae635ac37a7ef6ef1
Minh Dat <ngominhdat2004@gmail.com>
2026-02-14 02:21:51 +0700

    Tính nằng phân trang

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -37,3 +37,20 @@ if(formSearch){
         window.location.href = url.href;
     })
 }
+//End Search
+
+//Pagination
+const buttonPagination = document.querySelectorAll("[button-pagination]")
+if(buttonPagination){
+    let url = new URL(window.location.href);
+    
+
+    buttonPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+            url.searchParams.set("page", page);
+            window.location.href = url.href;
+        })
+    })
+}
+//End Pagination
