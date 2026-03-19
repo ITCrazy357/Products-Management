618a8de3b924a7caf44c3c009192a897c28204a2
Minh Dat <ngominhdat2004@gmail.com>
2026-03-19 22:16:50 +0700

    Update Tinymce

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,5 +1,25 @@
 tinymce.init({
   selector: "textarea.textarea-mce",
-  plugins: "image code table lists link media",
-  toolbar: "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | image media | code",
+  plugins: "image",
+  file_picker_callback: function (callback, value, meta) {
+    if (meta.filetype === "image") {
+      var input = document.createElement("input");
+      input.setAttribute("type", "file");
+      input.setAttribute("accept", "image/*");
+      input.onchange = function () {
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onload = function () {
+          var id = "blobid" + new Date().getTime();
+          var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+          var base64 = reader.result.split(",")[1];
+          var blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+          callback(blobInfo.blobUri(), { title: file.name });
+        };
+        reader.readAsDataURL(file);
+      };
+      input.click();
+    }
+  }
 });
