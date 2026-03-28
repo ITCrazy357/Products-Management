635d23aa1eef34c2f9c683d979eb930a08c03c4a
Minh Dat <ngominhdat2004@gmail.com>
2026-03-28 23:26:35 +0700

    12.1 Logout

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -2,39 +2,44 @@ const Account = require("../../models/account.model");
 const md5 = require("md5");
 const systemConfig = require("../../config/system");
 
-
 //[GET] /admin/auth/login
-module.exports.login = (req, res) =>{
-    res.render("admin/pages/auth/login", {
-        pageTitle: "Đăng nhập"
-    })
-}
+module.exports.login = (req, res) => {
+  res.render("admin/pages/auth/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
 
 //[POST] /admin/auth/login
-module.exports.loginPost = async (req, res) =>{
-    const email = req.body.email;
-    const password = req.body.password;
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const user = await Account.findOne({
-        email: email,
-        password: md5(password),
-        deleted: false
-    })
+  const user = await Account.findOne({
+    email: email,
+    password: md5(password),
+    deleted: false,
+  });
 
-    if(!user){
-        req.flash("error", "Email hoặc mật khẩu không đúng!")
-        res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
-        return;
-    }
+  if (!user) {
+    req.flash("error", "Email hoặc mật khẩu không đúng!");
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
 
-    if(user.status !== "active"){
-        req.flash("error", "Tài khoản của bạn đã bị khóa!")
-        res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
-        return;
-    }
+  if (user.status !== "active") {
+    req.flash("error", "Tài khoản của bạn đã bị khóa!");
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    return;
+  }
 
-    req.session.user = user; // Giữ lại gán session dự phòng nếu hệ thống bạn vẫn dùng
-    res.cookie("token", user.token)
-    res.redirect(`${systemConfig.prefixAdmin}/dashboard`)
+  req.session.user = user; // Giữ lại gán session dự phòng nếu hệ thống bạn vẫn dùng
+  res.cookie("token", user.token);
+  res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+};
 
-}
+//[GET] /admin/auth/logout
+module.exports.logout = (req, res) => {
+  req.session.destroy();
+  res.clearCookie("token");
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+};
