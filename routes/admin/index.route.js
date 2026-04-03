f55bc2983670c229b37a75b2ce02f853c5693344
Minh Dat <ngominhdat2004@gmail.com>
2026-04-03 23:19:17 +0700

    16. Xem và cập nhật thông tin cá nhân

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -6,6 +6,7 @@ const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
+const myAccountRoute = require("./my-account.route");
 
 
 module.exports = (app) => {
@@ -22,4 +23,6 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/accounts", authMiddleware.requireAuth, accountRoutes);
 
   app.use(PATH_ADMIN + "/auth", authRoutes);
+
+  app.use(PATH_ADMIN + "/my-account", authMiddleware.requireAuth, myAccountRoute);
 };
