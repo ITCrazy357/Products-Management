e7f32e85d58a7d13b25120ba8e08dd973f0ebd21
Minh Dat <ngominhdat2004@gmail.com>
2026-04-09 18:51:27 +0700

    27. làm trang đặt hàng thành công

---
+++ controllers/client/checkout.controller.js
@@ -0,0 +1,99 @@
+const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+const productHelper = require("../../helpers/products");
+const Order = require("../../models/order.model");
+
+//[GET] /checkout/
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({
+        _id: productId,
+      }).select("title thumbnail slug price discountPercentage");
+
+      productInfo.priceNew = productHelper.priceNewProduct(productInfo);
+
+      item.productInfo = productInfo;
+
+      item.totalPrice = item.quantity * productInfo.priceNew;
+    }
+  }
+
+  cart.totalPrice = cart.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0,
+  );
+
+  res.render("client/pages/checkout/index", {
+    pageTitle: "Thanh Toán",
+    cartDetail: cart,
+  });
+};
+
+//[GET] /checkout/order
+module.exports.order = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const userInfo = req.body;
+
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+
+  const products = [];
+  for (const product of cart.products) {
+    const objectProduct = {
+      product_id: product.product_id,
+      price: 0,
+      discountPercentage: 0,
+      quantity: product.quantity,
+    };
+
+    const productInfo = await Product.findOne({
+      _id: product.product_id,
+    }).select("price discountPercentage");
+
+    objectProduct.price = productInfo.price;
+    objectProduct.discountPercentage = productInfo.discountPercentage;
+
+    products.push(objectProduct);
+  }
+
+  const orderInfo = {
+    cart_id: cartId,
+    user_info: userInfo,
+    products: products,
+  };
+
+  const order = new Order(orderInfo);
+  await order.save();
+
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    {
+      products: [],
+    },
+  );
+
+  req.flash("success", "Đặt hàng thành công");
+
+  res.redirect(`/checkout/success/${order.id}`);
+};
+
+
+//[GET] /checkout/success/:orderId
+module.exports.success = async (req, res) => {
+    console.log(req.params.orderId)
+
+    res.render("client/pages/checkout/success", {
+        pageTitle: "Thanh toán thành công"
+    })
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,8 @@ const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
+
 
 
 module.exports = (app) => {
@@ -18,4 +20,6 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
 
   app.use("/cart", cartRoutes);
+
+  app.use("/checkout", checkoutRoutes);
 };
