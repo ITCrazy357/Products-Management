0c5da0d66388d5d08790d4765c6f75bc35f1298a
Minh Dat <ngominhdat2004@gmail.com>
2026-04-15 01:57:59 +0700

    39.4 Chèn Icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,6 @@
+import * as Popper from "https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js";
+import "https://cdn.jsdelivr.net/npm/emoji-picker-element@^1/index.js";
+
 //CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
@@ -47,3 +50,30 @@ if (bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 //END Sroll chat to buttom
+
+//show icon chat
+
+//Show popup
+const buttonIcon = document.querySelector(".button-icon");
+if (buttonIcon) {
+  const tooltip = document.querySelector(".tooltip");
+  Popper.createPopper(buttonIcon, tooltip);
+
+  buttonIcon.onclick = () => {
+    tooltip.classList.toggle("shown");
+  };
+}
+//END Show popup
+
+const emojiPicker = document.querySelector("emoji-picker");
+if (emojiPicker) {
+  const inputChat = document.querySelector(
+    ".chat .inner-form input[name='content']",
+  );
+  emojiPicker.addEventListener("emoji-click", (event) => {
+    const icon = event.detail.unicode; // Lấy ký tự emoji
+    inputChat.value = inputChat.value + icon; // Thêm vào sau nội dung cũ trong input
+  });
+}
+
+//end show icon chat
