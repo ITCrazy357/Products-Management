1e57d325c1c22fe98ba2efd34a2d76f16c854b0c
Minh Dat <ngominhdat2004@gmail.com>
2026-04-16 23:27:14 +0700

    40.1 Typing phần 2(finish)

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ if (formSendData) {
     if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
       e.target.elements.content.value = "";
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
@@ -19,11 +20,12 @@ if (formSendData) {
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
   const body = document.querySelector(".chat .inner-body");
+  const boxTyping = document.querySelector(".chat .inner-list-typing");
 
   const div = document.createElement("div");
   let htmlFullName = "";
 
-  if (myId === data.userId) {
+  if (myId === data.user_id) {
     div.classList.add("inner-outgoing");
   } else {
     htmlFullName = `<div class="inner-name">${data.fullname}</div>`;
@@ -36,7 +38,7 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     <div class="inner-content">${data.content}</div>
   `;
 
-  body.appendChild(div);
+  body.insertBefore(div, boxTyping);
 
   // Tự động cuộn xuống đáy khi có tin nhắn mới
   body.scrollTop = body.scrollHeight;
@@ -65,6 +67,20 @@ if (buttonIcon) {
 }
 //END Show popup
 
+//Show Typing
+var timeOut;
+
+const showTyping = () => {
+  socket.emit("CLIENT_SEND_TYPING", "show");
+
+  clearTimeout(timeOut);
+
+  timeOut = setTimeout(() => {
+    socket.emit("CLIENT_SEND_TYPING", "hidden");
+  }, 3000);
+};
+//END Show Typing
+
 const inputChat = document.querySelector(
   ".chat .inner-form input[name='content']",
 );
@@ -74,13 +90,25 @@ if (emojiPicker && inputChat) {
   emojiPicker.addEventListener("emoji-click", (event) => {
     const icon = event.detail.unicode; // Lấy ký tự emoji
     inputChat.value = inputChat.value + icon; // Thêm vào sau nội dung cũ trong input
+
+    const end = inputChat.value.length;
+
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+
+    showTyping();
   });
 }
 
 //input keyup
+
 if (inputChat) {
   inputChat.addEventListener("keyup", () => {
     socket.emit("CLIENT_SEND_TYPING", "show");
+
+    clearTimeout(timeOut);
+
+    showTyping();
   });
 }
 // END input keyup
@@ -88,7 +116,42 @@ if (inputChat) {
 //end show icon chat
 
 //SERVER_RETURN_TYPING
-socket.on("SERVER_RETURN_TYPING", (data) => {
-  console.log(data);
-});
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+
+if (elementListTyping) {
+  socket.on("SERVER_RETURN_TYPING", (data) => {
+    if (data.type == "show") {
+      // Check if typing indicator for this user already exists
+      const existTyping = elementListTyping.querySelector(
+        `[user-id='${data.user_id}']`,
+      );
+
+      if (!existTyping) {
+        const bodyChat = document.querySelector(".chat .inner-body");
+        const boxTyping = document.createElement("div");
+        boxTyping.classList.add("box-typing");
+        boxTyping.setAttribute("user-id", data.user_id);
+
+        boxTyping.innerHTML = `
+          <div class="inner-name">${data.fullname}</div>
+          <div class="inner-dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          `;
+        elementListTyping.appendChild(boxTyping);
+        bodyChat.scrollTop = bodyChat.scrollHeight;
+      }
+    } else {
+      const boxTypingRemove = elementListTyping.querySelector(
+        `[user-id='${data.user_id}']`,
+      );
+      if (boxTypingRemove) {
+        elementListTyping.removeChild(boxTypingRemove);
+      }
+    }
+  });
+}
+
 //END SERVER_RETURN_TYPING
