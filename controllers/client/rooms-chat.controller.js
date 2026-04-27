62564ee6a9e90ac806b79e11f01d661f6b6692c3
Minh Dat <ngominhdat2004@gmail.com>
2026-04-28 00:49:45 +0700

    46.2 Tạo phòng chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -1,5 +1,58 @@
+const User = require("../../models/user.model");
+const RoomChat = require("../../models/rooms-chat.model");
+
 //[GET] /rooms-chat/
 module.exports.index = async (req, res) => {
-  res.render("client/pages/rooms-chat/index");
-  pageTitle: "Danh sách phòng chat";
+  res.render("client/pages/rooms-chat/index", {
+    pageTitle: "Danh sách phòng chat",
+  });
+};
+
+//[GET] /rooms-chat/create
+module.exports.create = async (req, res) => {
+  const friendList = res.locals.userInfo.friendList;
+
+  for (const friend of friendList) {
+    const infoFriend = await User.findOne({
+      _id: friend.user_id,
+      deleted: false,
+    }).select("fullname avatar ");
+
+    friend.infoFriend = infoFriend;
+  }
+  res.render("client/pages/rooms-chat/create", {
+    pageTitle: "Tạo phòng chat",
+    friendList: friendList,
+  });
+};
+
+//[POST] /rooms-chat/create
+module.exports.createPost = async (req, res) => {
+  const title = req.body.title;
+  const usersId = req.body.usersId;
+
+  const dataRoom = {
+    title: title,
+    typeRoom: "group",
+    users: [],
+  };
+
+  for (const userId of usersId) {
+    dataRoom.users.push({
+      user_id: userId,
+      role: "user",
+    });
+  }
+
+  dataRoom.users.push({
+    user_id: res.locals.userInfo.id,
+    role: "superAdmin",
+  });
+
+  const roomChat = new RoomChat(dataRoom);
+  await roomChat.save();
+
+  req.flash("success", "Tạo phòng chat thành công");
+
+  res.redirect(`/chat/${roomChat.id}`);
 };
